bd8dfc0edba261b245583205e2643dbfee1f7b15
Garrett <gmccabe@gmail.com>
2020-03-25 14:04:22 -0500

    Changes to web frontend

--- public/inc/header.php
+++ public/inc/header.php
@@ -1,17 +1,18 @@
 <html>
 <head>
-    <title>Buttscribe</title>
+    <title>40K Sanctum</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 </head>
 <body>
 
 <nav class="navbar navbar-inverse navbar-default">
     <div class="container">
-        <div class="navbar-header"><a class="navbar-brand" href="/">ButtScribe</a></div>
+        <div class="navbar-header"><a class="navbar-brand" href="/">40K Sanctum</a></div>
         <div id="navbar" class="collapse navbar-collapse">
             <ul class="nav navbar-nav">
-                <li><a href="/40k.php">40k (8th)</a></li>
-                <li><a href="/kt.php">Kill Team (2018)</a></li>
+                <li><a href="/40k.php">40k Roster Card Builder (Buttscribe)</a></li>
+                <!-- <li><a href="/kt.php">Kill Team (2018)</a></li> -->
+                <li><a href="https://cloud.gmccabe.com/s/tJ26x9GDdcjnxeY">Community Share Folder</a></li>
               </ul>
         </div><!--/.nav-collapse -->
     </div>
