c18bfdf2aca88999911b7d786b1ba99a81b9bb02
Garrett <gmccabe@gmail.com>
2020-03-24 19:33:11 -0500

    Fixed annoying button label

--- public/40k.php
+++ public/40k.php
@@ -23,7 +23,7 @@
         </div>
         <div class="form-group">
             <div class="col-sm-offset-2 col-sm-10">
-                <input type="submit" value="pres" class="btn btn-default">
+                <input type="submit" value="go!" class="btn btn-default">
             </div>
         </div>
         </form>
